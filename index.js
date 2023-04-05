5992fe65c138681af3f61992be413727b5eee2ed
Ikenna Richard <oguejioforrichard@gmail.com>
2023-04-04 13:46:57 -1100

    Add test for string length function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // returns string length or error
-const stringLength = (str) => str.length >= 1 && str.length  <= 10 ? true : 'Error: str not between 1 and 10';
+const stringLength = (str) => str.length >= 1 && str.length  <= 10 ? str.length : 'Error: str not between 1 and 10';
 
-module.exports = stringLength;
+module.exports = {stringLength, reverseString};
--- index.test.js
+++ index.test.js
@@ -1,6 +1,13 @@
 const stringLength = require('./index');
 
-test('returns the character count', () => {
-  expect(stringLength('word')).toBe(4);
-  expect(stringLength('microverse')).toBe(10);
+test('return count if string length is between 1 nad 10 ', () => {
+  const str = 'Microverse';
+  const len = str.length;
+  expect(stringLength(str)).toBe(len);
+});
+
+test('return error if string length is not between 1 nad 10 ', () => {
+  const str = '';
+  const error = 'Error: str not between 1 and 10';
+  expect(stringLength(str)).toBe(error);
 });
